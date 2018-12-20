5151454fc29d744d15aed6a15c7c2d7972ca316c
Fiona Voss <fiona.voss@redbubble.com>
2018-12-19 16:16:51 -0800

    Show coordinates of each move

--- src/game.js
+++ src/game.js
@@ -47,9 +47,14 @@ export default class Game extends React.Component {
         'Go to move #' + move :
         'Go to game start';
       const disabled = this.state.stepNumber === move;
+
+      const position = move ? getPosition(step.squares, history[move - 1].squares) : null;
+      const coordinates = move ? coordinateString(position) : "";
+
       return(
         <li key={move}>
           <button onClick={() => this.jumpTo(move)} disabled={disabled}>{desc}</button>
+          {coordinates}
         </li>
       );
     });
@@ -96,4 +101,18 @@ function calculateWinner(squares) {
     }
   }
   return null;
+}
+
+function coordinateString(position) {
+  const column = Math.floor(position / 3) + 1;
+  const row = (position) % 3 + 1;
+  return "(" + column + ", " + row + ")";
+}
+
+function getPosition(currentSquares, previousSquares) {
+  for (let i = 0; i < currentSquares.length; i++) {
+    if (currentSquares[i] !== previousSquares[i]) {
+      return i;
+    }
+  }
 }
